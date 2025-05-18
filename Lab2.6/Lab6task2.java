6ffe80827b1eebfc51518e6485b4015ac27dc865
Vlad2713 <mister.vladinator2014@gmail.com>
2025-05-18 17:47:36 +0300

    Видалити всі входження th

--- Lab2.6/Lab6task2.java
+++ Lab2.6/Lab6task2.java
@@ -1,3 +1,13 @@
+import java.util.Scanner;
+
 public class Lab6task2 {
-    
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введіть текст:");
+        String input = scanner.nextLine();
+
+        String result = input.replace("th", "");
+        System.out.println("Результат після видалення \"th\":");
+        System.out.println(result);
+    }
 }
